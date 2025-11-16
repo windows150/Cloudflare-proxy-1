157f8558d39b67a1483ff5bb0a0c71ac6c91fbce
windows150 <wangjianlin2029@gmail.com>
2025-11-16 17:13:51 +0800

    Update password UUID in shadowsocks.js

--- shadowsocks.js
+++ shadowsocks.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 let subPath = 'link';     // 节点订阅路径,不修改将使用UUID作为订阅路径
 let proxyIP = '13.230.34.30';  // proxyIP 格式：ip、域名、ip:port、域名:port等,没填写port，默认使用443
-let password = '5dc15e15-f285-4a9d-959b-0e4fbdd77b63';  // 节点UUID
+let password = '151069c3-b0f3-4361-9013-858dafa7d3cb';  // 节点UUID
 
 // CF CDN 
 let cfip = [ // 格式:优选域名:端口#备注名称、优选IP:端口#备注名称、[ipv6优选]:端口#备注名称、优选域名#备注 
